5adcd013c45648ceecaac6f352ef9ae6bfe12957
tmin002 <ssh9930@gmail.com>
2024-12-10 13:32:48 +0900

    createElement: add events to self/children

--- learnfastapi-client/src/javascript/element/DOM.js
+++ learnfastapi-client/src/javascript/element/DOM.js
@@ -1,9 +1,6 @@
 export function E(element_id) {
     return document.getElementById(element_id);
 }
-export function C(element_class) {
-    return document.getElementsByClassName(element_id)[0];
-}
 export function setElementDisplay(element, display) {
     element.style.display = display ? display : 'none';
 }
@@ -11,8 +8,13 @@ export function setElementDisplay(element, display) {
 export function createElement(tagName, innerHTML, events = {}) {
     let element = document.createElement(tagName);
     element.innerHTML = innerHTML;
-    Object.keys(events).forEach(eventName => {
-        element[eventName] = events[eventName];
-    })
+
+    Object.keys(events).forEach(querySelector => {
+        let target = querySelector === '$self' ?
+            element : element.querySelector(querySelector);
+        target[events[querySelector]['eventName']]
+         = events[querySelector]['event'];
+    });
+
     return element;
 }
